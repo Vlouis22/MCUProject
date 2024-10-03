6d697104d50cb02895c1d52301fb21d616f6f228
Valery Louis <louisvalery2022@gmail.com>
2024-10-02 22:23:36 -0400

    update

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ import pg from 'pg'
 
 const config = {
     user: process.env.PGUSER || 'postgres',
-    password: process.env.PGPASSWORD || 'moXzVDAKmLhcAyHWkUIvGXiFbQHuuYIm',
+    password: process.env.PGPASSWORD,
     host: process.env.PGHOST || 'junction.proxy.rlwy.net',
     port: process.env.PGPORT || 41390,
     database: process.env.PGDATABASE || 'railway'
